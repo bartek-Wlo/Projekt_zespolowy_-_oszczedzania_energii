309b6c7f6409f5be460f819d96ca15f761eca68b
bwlodarc <b748462847@gmail.com>
2025-06-07 20:13:48 +0200

    ESP 8266 Wemos D1 R1

---
+++ ESP_wemos.c
@@ -0,0 +1,11 @@
+void setup() {
+  WiFi.mode(WIFI_OFF); // wyłącz Wi-Fi
+  WiFi.forceSleepBegin(); // wejdź w tryb oszczędzania
+  delay(10); // potrzebna mała przerwa
+  // ESP.deepSleep(2000); 
+  return;
+}
+
+void loop() {
+  return;
+}
